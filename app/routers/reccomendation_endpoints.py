f937182b545146ad0c4619bce81f394640c3cab6
MelDxKviel <mega1hack52@gmail.com>
2024-06-10 23:37:28 +0700

    fix typos

--- app/routers/reccomendation_endpoints.py
+++ app/routers/reccomendation_endpoints.py
@@ -5,24 +5,24 @@ from app.models import Recommendation, RecommendationRead
 from app.database import get_session
 
 
-reccomendation_router = APIRouter(
-    prefix="/reccomendations",
-    tags=["Reccomendations"],
+recommendation_router = APIRouter(
+    prefix="/recommendations",
+    tags=["Recommendations"],
     responses={404: {"description": "Not found"}},
 )
 
 
-@reccomendation_router.get("/")
-async def get_reccomendations(session: Session = Depends(get_session)) -> list[RecommendationRead]:
-    reccomendations = session.exec(select(Recommendation)).all()
-    return reccomendations
+@recommendation_router.get("/")
+async def get_recommendations(session: Session = Depends(get_session)) -> list[RecommendationRead]:
+    recommendations = session.exec(select(Recommendation)).all()
+    return recommendations
 
 
-@reccomendation_router.get("/{reccomendation_id}")
-async def get_reccomendation_by_id(reccomendation_id: int, session: Session = Depends(get_session)) -> RecommendationRead:
-    reccomendation = session.exec(select(Recommendation).where(
-        Recommendation.id == reccomendation_id)
+@recommendation_router.get("/{recommendation_id}")
+async def get_recommendation_by_id(recommendation_id: int, session: Session = Depends(get_session)) -> RecommendationRead:
+    recommendation = session.exec(select(Recommendation).where(
+        Recommendation.id == recommendation_id)
     ).first()
-    if not reccomendation:
+    if not recommendation:
         raise HTTPException(status_code=404, detail="Reccomendation not found")
-    return reccomendation
+    return recommendation
